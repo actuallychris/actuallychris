8575ba98e08926da32d036be3c8a0a6719e374f0
Christopher Smith <actuallychris@gmail.com>
2017-03-29 13:56:23 -0600

    new update

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,64 @@
-$(function(){
+/* Make Each author have his own array */
+
+console.log("The scripts have started!");
+
+loadJSON();
+
+function loadJSON() {
+    console.log("Reading the JSON file.");
+    $.ajax({
+        url: "/js/quotes.json",
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            loadAuthorList(data);
+
+        }
+    });
+}
+
+function loadAuthorList(data) {
+    console.log("Loading Author List");
+
+    var listHTML = "";
+    for(var i = 0; i < data.length; i++) {
+        listHTML += "<a href=\"javascript:loadAuthor(" + i + ")\"><div>" + data[i].author + "</div></a>";
+    }
+    $("#authorList").html(listHTML);
+}
+
+$(function () {
     // All jQuery code goes here
-    $('.dropdownMenu > li').hover(function() {
+    $('.dropdownMenu > li').hover(function () {
         $(this).children("ul").slideToggle(200);
     })
 })
+
+jQuery(document).ready(function ($) {
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //var animalArray  = Object.keys(animals);
 //var randomNumber = Math.random();
