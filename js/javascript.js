403e34603ee1547685dae63119e4dd9c65cb3fdf
Christopher Smith <actuallychris@gmail.com>
2017-03-29 05:09:38 -0600

    new commit

--- js/javascript.js
+++ js/javascript.js
@@ -4,3 +4,29 @@ $(function(){
         $(this).children("ul").slideToggle(200);
     })
 })
+
+//var animalArray  = Object.keys(animals);
+//var randomNumber = Math.random();
+//var animalIndex  = Math.floor(randomNumber * animalArray.length);
+//
+//var randomKey    = animalArray[animalIndex];
+// This will course this will return the value of the randomKey
+// instead of a fresh random value
+//var randomValue  = animals[randomKey];
+
+
+/*
+var animals = {
+    'cat': 'meow',
+    'dog': 'woof',
+    'cow': 'moo',
+    'sheep': 'baaah',
+    'bird': 'tweet'
+};
+
+// Random Key
+console.log(Object.keys(animals)[Math.floor(Math.random()*Object.keys(animals).length)]);
+
+// Random Value
+console.log(animals[Object.keys(animals)[Math.floor(Math.random()*Object.keys(animals).length)]]);
+*/
