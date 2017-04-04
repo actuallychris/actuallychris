cfe91211c3dcdf29081bb63238d1a19367547bf3
Christopher Smith <actuallychris@gmail.com>
2017-04-03 20:15:39 -0600

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -13,67 +13,61 @@ jQuery(document).ready(function ($) {
 
 })
 
-loadJSON();
+function search(nameVal, myArray) {
+    var newArray = [];
+    for (var i = 0; i < myArray.length; i++) {
+        console.log(myArray[i].author);
+        /*if (myArray[i].author.split(" ").pop() === nameVal) {
+            newArray.push(myArray[i]);
+        }
+        else if (myArray[i].topic.split(" ").pop() === nameVal) {
+            newArray.push(myArray[i]);
+        }*/
+    }
+    return newArray;
+}
 
-function loadJSON() {
+
+
+$(function () {
     console.log("Reading the JSON file.");
-    $.ajax({
-        url: "/js/quotes.json",
-        contentType: 'application/json; charset=UTF-8',
-        dataType: "json",
-        success: function (data) {
-            console.log(data);
-            open("GET", "/js/quotes.json", true);
-            document.getElementById("author").innerHTML = Author;
-            var Quote = data["Quote"];
-            var Author = data["Author"];
-                document.getElementById("quote").innerHTML = Quote;
-            console.log(Quote);
-            console.log(Author);
-        },
+    $.getJSON("../../js/quotes.json", function (data) {
+        console.log(data);
+        var classT = $("body").attr("class");
+        //console.log(classT);
+        var classA = String(classT).split(" ");
+        //console.log(classA[1]);
+        //var quoteArray = search(classA[1], data);
+        var quoteArray = [];
+        console.log('almost there');
+        if (classA[0] === "Author") {
+            quoteArray = data.filter(function (obj) {
+                return String(obj.author).split(" ").pop() === classA[1];
+            });
+        }
+        else if (classA[0] === "Topic") {
+            quoteArray = data.filter(function(obj){
+                return obj.topic.indexOf(classA[1]) > -1;
+            });
+        }
+
+        console.log(quoteArray);
+        var resourceQ = Quotes(quoteArray);
+        var final_quote = resourceQ.quote;
+        var final_author = resourceQ.author;
+        var final_topic = resourceQ.topic;
+        console.log(final_quote);
+        console.log(final_author);
+        console.log(final_topic);
+
+        document.getElementById("Quote-div").innerHTML = final_quote;
+        document.getElementById("Author-div").innerHTML = final_author;
+        document.getElementById("Topic-div").innerHTML = final_topic;
     });
-    /*loadAuthorList(data);*/
-}
 
 
-/*
-for (var i in JSON_Object) {
-    for (var j in JSON_Object[i]){
-        console.log(JSON_Object[i][j].Author);
-    }
-}*/
-
-
-//Make sure that something is in the array
-
-/*
-var index = 0;
-var outerArray = JSON.parse(jsonText);
-var map = {};
-var ids = [    // This assumes no more than 99 elements
-    "#one", "#two", "#three",...So on & So forth..., "#ninety-nine"
-];x
-outerArray.forEach(function(nestedArray) {
-    nestedArray.forEach(function(object) {
-        map[ids[index++]] = object;
-        delete object.count; // Doesn't matter whether this is before or after the line above
-    });
-});
-/*
-/*function newQuote() {
-    var quoteID = Math.floor(Math.random() * (quotes.length));
-    $("#quote").html(quotes[quoteID].quote);
-    $("#attrib").html(quotes[quoteID].attrib);
-    $("#twitterButton").attr("href", "http://twitter.com/intent/tweet?text=" + quotes[quoteID].quote + " -" + quotes[quoteID].attrib);
-}
-
-// wait for page load before displaying content
-$(document).ready(function () {
-    // load an initial quote
-    newQuote();
 
 });
-*/
 
 
 
@@ -82,30 +76,10 @@ $(document).ready(function () {
 
 
 
-/*function loadAuthorList(data) {
-    console.log("Loading Author List");
-
-    var listHTML = "";
-    for(var i = 0; i < data.length; i++) {
-        listHTML += "<a href=\"javascript:loadAuthor(" + i + ")\"><div>" + data[i].author + "</div></a>";
-    }
-    $("#authorList").html(listHTML);
-}
-*/
-
-
-
-
-function Quotes() {
-
-
-    var quoteArray = Object.keys(id);
+function Quotes(quoteArray2) {
     var randomNumber = Math.random();
-    var fullQuoteIndex = Math.floor(randomNumber * quoteArray.length);
-
-    var randomKey = quoteArray[fullQuoteIndex];
-
-
+    var fullQuoteIndex = Math.floor(randomNumber * quoteArray2.length);
+    return quoteArray2[fullQuoteIndex];
 };
 
 
@@ -125,18 +99,8 @@ function Quotes() {
 // instead of a fresh random value
 //var randomValue  = animals[randomKey];
 
-
 /*
-var animals = {
-    'cat': 'meow',
-    'dog': 'woof',
-    'cow': 'moo',
-    'sheep': 'baaah',
-    'bird': 'tweet'
-};
-
-
-// Random Key
+ Random Key
 console.log(Object.keys(animals)[Math.floor(Math.random()*Object.keys(animals).length)]);
 
 // Random Value
